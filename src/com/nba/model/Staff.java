da5a077104b75332bdc2b1f54740985c3a7ae1df
Nikita Hordiienko <mhordiienko23@gmail.com>
2026-04-13 10:42:56 +0300

    docs: Initial architecture and abstract Staff class

---
+++ src/com/nba/model/Staff.java
@@ -0,0 +1,42 @@
+package com.nba.model;
+
+import java.io.Serializable;
+
+public abstract class Staff implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static int idCounter = 1;
+    private final int id;
+    private String name;
+    private double baseSalary;
+
+    public Staff( String name, double baseSalary){
+        this.id = idCounter++;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public abstract double calculateBonus();
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("ID: %03d | Name: %-15s | Base: $%,.0f", id, name, baseSalary);
+    }
+}
